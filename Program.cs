519940109c4d52bab74ece45ba0746e3bdc04111
Hilet Aulas <hilet@HILET.LOCAL>
2024-10-07 08:37:28 -0300

    bd y authorization

--- Program.cs
+++ Program.cs
@@ -32,6 +32,10 @@ builder.Services.AddCors(options =>
 }
 );
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireClaim("tipo_usuario", "admin"));
+});
 
 builder.Services.AddControllers();
 
@@ -50,8 +54,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
-
 app.UseAuthorization();
+
 app.MapControllers();
 app.UseCors();
 
