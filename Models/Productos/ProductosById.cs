22dd2498d3dcf1b6bae07dada7efee74de88b54c
Luciano <lucianoceles@gmail.com>
2024-12-01 16:31:38 -0300

    cache de items

---
+++ Models/Productos/ProductosById.cs
@@ -0,0 +1,14 @@
+﻿namespace  MYABackend.Models.Productos;
+public class ProductosById
+{
+    public int id_producto { get; set; }
+    public int id_categoria { get; set; }
+    public int id_marca { get; set; }
+    public string nombre { get; set; } = string.Empty;
+    public decimal precio { get; set; }
+    public string descripcion { get; set;} = string.Empty;
+    public string descripcion_marca { get; set;} = string.Empty;
+    public string descripcion_categoria { get; set; } = string.Empty;
+    public string url_imagen { get; set; } = string.Empty;
+    public string talle { get; set; } = string.Empty;
+}
