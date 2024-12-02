cfa66f212953825e7283abd949f02438467e2357
Luciano <lucianoceles@gmail.com>
2024-12-01 23:17:23 -0300

    arreglo de errores

--- Models/Productos/ProductosById.cs
+++ Models/Productos/ProductosById.cs
@@ -6,7 +6,7 @@ public class ProductosById
     public int id_marca { get; set; }
     public string nombre { get; set; } = string.Empty;
     public decimal precio { get; set; }
-    public string descripcion { get; set;} = string.Empty;
+    public string descripcion_producto { get; set;} = string.Empty;
     public string descripcion_marca { get; set;} = string.Empty;
     public string descripcion_categoria { get; set; } = string.Empty;
     public string url_imagen { get; set; } = string.Empty;
